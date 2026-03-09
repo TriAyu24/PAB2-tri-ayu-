913f76e27e21f2a8756d2652003b62b1bee30d7c
mutiara <triayumiranda2004@gmail.com>
2026-03-09 15:43:31 +0700

    Terbaru

---
+++ pilem/lib/screens/favorite_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:pilem/models/movie.dart';
+import 'package:pilem/services/api_service.dart';
+import 'detail_screen.dart';
+
+// Favorite scereen menggunakan StatefulWidget karena data bisa berubah (misal: user menambah/menghapus favorit)
+class FavoriteScreen extends StatefulWidget {
+  const FavoriteScreen({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    throw UnimplementedError();
+  }
+}
